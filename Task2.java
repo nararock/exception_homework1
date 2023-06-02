cf8609998c5fdf68fe7b7dad68df87194628aae8
Elena Gerasimovich <nararock@mail.ru>
2023-06-02 15:36:22 +0300

    changed Task2 and Task3

--- Task2.java
+++ Task2.java
@@ -1,8 +1,4 @@
 public class Task2 {
-    public static void main(String[] args) {
-        int m = Integer.MAX_VALUE + 1;
-    }
-
     public static int sum2d(String[][] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
